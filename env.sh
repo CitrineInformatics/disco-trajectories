fd37c0baa2115a602de369615df0431dbb95d9a1
Vinay Hegde <vhegde@citrine.io>
2021-03-18 12:29:04 -0700

    update env script to use pip for pymatgen

--- env.sh
+++ env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86-64.sh -b
+bash Miniconda3-latest-Linux-x86_64.sh -b
+source miniconda3/bin/activate
 echo `which conda`
-conda create -n disco
-conda activate disco
+conda create -n disco python --yes && conda activate disco
+echo `which python`
 conda install --yes numpy scipy matplotlib scikit-learn
-conda install --yes --channel conda-forge pymatgen=2021.2.16
-conda install --yes --channel conda-forge matminer
+pip install pymatgen==2021.2.16 matminer==0.6.5
+conda install git
+git clone https://github.com/CitrineInformatics/disco-trajectories.git
+cd disco-trajectores/src/discworld && python pso.py
